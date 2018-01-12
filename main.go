ac411f01afeb437b1df0773ed2190a20d600de52
Nqc <24802117@qq.com>
2018-01-12 17:41:17 +0800

    XRtspServer的测试项目
    XRtspServer的测试项目，增加了获取目前服务器摄像头的列表及对应的观看者IP的列表

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"nqc.cn/XRtspServer/stream_server"
+	"nqc.cn/server"
+	"nqc.cn/server/gin"
+	"nqc.cn/XRtspServer/rtsp"
+	"net/http"
+	"encoding/json"
+	"nqc.cn/XRtspServer/RtspClientManager"
+)
+
+var rtspServer *rtsp.RtspServer
+func main() {
+
+
+	s := stream_server.NewStreamServer(":8554")
+
+
+	var err error
+	rtspServer, err = s.NewRtspServer()
+	if err != nil {
+		return
+	}
+
+
+	s.Run(rtspServer)
+
+	ser := server.Default()//服务器初始化
+	ser.GET("getRtspList",getRtspList)
+	ser.GET("getUserList",getUserList)
+
+
+	s.Wrap(ser.RunServer)
+
+}
+
+func getRtspList(c *gin.Context) {
+
+	list := RtspClientManager.GetCurrManagers()
+
+	body,_ := json.Marshal(list)
+
+	c.String(http.StatusOK,string(body))
+
+
+}
+
+func getUserList(c *gin.Context) {
+
+	//list := rtspServer.GetUserList()
+
+	//body,_ := json.Marshal(list)
+
+	c.String(http.StatusOK,"getUserList")
+
+}
